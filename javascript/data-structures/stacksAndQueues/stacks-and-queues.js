ca1b89fcee677b8a08a56c471b9052b0a1892929
Alexander Whan <whan.alexander@gmail.com>
2020-08-21 14:02:34 -0700

    Added basic tests and guideline comments

--- javascript/data-structures/stacksAndQueues/stacks-and-queues.js
+++ javascript/data-structures/stacksAndQueues/stacks-and-queues.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// Create a Node class that has properties for the value stored in the Node, and a pointer to the next node.
+
 class Node {
   constructor(value, next = null) {
     this.value = value;
@@ -7,24 +9,60 @@ class Node {
   }
 }
 
-class Stack {}
+// Create a Stack class that has a top property. It creates an empty Stack when instantiated.
+
+// This object should be aware of a default empty value assigned to top when the stack is created.
+
+// the way insert worked on our LL is how PUSH works here
+// Think of STACKS like a LINKED LIST that we're INSERTING
+// Think about the bottom pancake having a NEXT of NULL, and every PANCAKE you put on is like our regular LL's insert function
+
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+
+  // Establish NODE and assign next
+  // Define a method called push which takes any value as an argument and adds a new node with that value to the top of the stack with an O(1) Time performance.
+  push(value) {
+    return (this.top = new Node(value, this.top));
+  }
+
+  // Define a method called pop that does not take any argument, removes the node from the top of the stack, and returns the node’s value.
+  // Should raise exception when called on empty stack
+  pop() {
+    if (this.isEmpty()) {
+      throw new RangeError('Cannot pop off an empty stack');
+    }
+
+    return this.top.pop();
+    // this.top.next?
+  }
+
+  // Define a method called peek that does not take an argument and returns the value of the node located on top of the stack, without removing it from the stack.
+  // Should raise exception when called on empty stack
+  peek() {
+    if (this.isEmpty()) {
+      throw new RangeError('Cannot peek at an empty stack');
+    }
+    return this.top.value;
+  }
+
+  // Define a method called isEmpty that takes no argument, and returns a boolean indicating whether or not the stack is empty.
+  isEmpty() {
+    if (!this.top) {
+      return true;
+    } else if (this.top.value) {
+      return false;
+    }
+  }
+}
 
 class Queue {}
 
 module.exports = Node;
 module.exports = Stack;
 module.exports = Queue;
-
-// Create a Node class that has properties for the value stored in the Node, and a pointer to the next node.
-
-// Create a Stack class that has a top property. It creates an empty Stack when instantiated.
-// This object should be aware of a default empty value assigned to top when the stack is created.
-// Define a method called push which takes any value as an argument and adds a new node with that value to the top of the stack with an O(1) Time performance.
-// Define a method called pop that does not take any argument, removes the node from the top of the stack, and returns the node’s value.
-// Should raise exception when called on empty stack
-// Define a method called peek that does not take an argument and returns the value of the node located on top of the stack, without removing it from the stack.
-// Should raise exception when called on empty stack
-// Define a method called isEmpty that takes no argument, and returns a boolean indicating whether or not the stack is empty.
 
 // Create a Queue class that has a front property. It creates an empty Queue when instantiated.
 // This object should be aware of a default empty value assigned to front when the queue is created.
--- javascript/data-structures/stacksAndQueues/stacks-and-queues.test.js
+++ javascript/data-structures/stacksAndQueues/stacks-and-queues.test.js
@@ -1,20 +1,48 @@
 'use strict';
 
+const Stack = require('./stacks-and-queues');
+const Node = require('./stacks-and-queues');
+
 // Write tests to prove the following functionality:
 
 // Can successfully push onto a stack
-// Can successfully push multiple values onto a stack
-// Can successfully pop off the stack
-// Can successfully empty a stack after multiple pops
-// Can successfully peek the next item on the stack
-// Can successfully instantiate an empty stack
-// Calling pop or peek on empty stack raises exception
-// Can successfully enqueue into a queue
-// Can successfully enqueue multiple values into a queue
-// Can successfully dequeue out of a queue the expected value
-// Can successfully peek into a queue, seeing the expected value
-// Can successfully empty a queue after multiple dequeues
-// Can successfully instantiate an empty queue
-// Calling dequeue or peek on empty queue raises exception
-// Ensure your tests are passing before you submit your solution.
 
+it('can successfully push onto a stack', () => {
+  const stack = new Stack();
+  stack.push());
+  expect(stack.peek()).toBe('apples');
+});
+
+// Can successfully push multiple values onto a stack
+
+// Can successfully pop off the stack
+
+// Can successfully empty a stack after multiple pops
+
+// Can successfully peek the next item on the stack
+
+// Can successfully instantiate an empty stack
+
+it('should instantiate an empty stack - aka not blow up', () => {
+  const stack = new Stack();
+  expect(stack).toBeDefined();
+  // Check if it's empty? How?
+});
+
+// Calling pop or peek on empty stack raises exception
+
+// Can successfully enqueue into a queue
+
+// Can successfully enqueue multiple values into a queue
+
+// Can successfully dequeue out of a queue the expected value
+
+// Can successfully peek into a queue, seeing the expected value
+
+// Can successfully empty a queue after multiple dequeues
+
+// Can successfully instantiate an empty queue
+
+// Calling dequeue or peek on empty queue raises exception
+
+// Ensure your tests are passing before you submit your solution.
