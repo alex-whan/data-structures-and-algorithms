f8c27f082b418721570e295222e2392373729e7f
Alexander Whan <whan.alexander@gmail.com>
2020-08-23 20:10:13 -0700

    Set up ll-zip file and finished zipLists challenge code

---
+++ javascript/challenges/llZip/ll-zip.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const LinkedList = require('../../data-structures/linkedList/linked-list.js');
+
+function zipLists(list1, list2) {
+  let zippedList = new LinkedList();
+  zippedList.insert(list1.head.value);
+  zippedList.append(list2.head.value);
+
+  let current = list1.head.next;
+  let current2 = list2.head.next;
+
+  while (current || current2) {
+    if (current) {
+      zippedList.append(current.value);
+      current = current.next;
+    }
+
+    if (current2) {
+      zippedList.append(current2.value);
+      current2 = current.next;
+    }
+  }
+
+  return zippedList;
+}
+
+module.exports = zipLists;
