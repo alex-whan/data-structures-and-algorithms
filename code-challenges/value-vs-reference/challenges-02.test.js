b92f31523fc70b982634790a67f73a5e82b5ab02
Alexander Whan <whan.alexander@gmail.com>
2020-11-12 15:46:35 -0800

    Solved stretch goal 6 of Challenge 2

--- code-challenges/value-vs-reference/challenges-02.test.js
+++ code-challenges/value-vs-reference/challenges-02.test.js
@@ -90,7 +90,7 @@ console.log(a) prints [1, 2, 3, 4]
 ------------------------------------------------------------------------------------------------ */
 
 const append = (arr1, arr2) => {
-  // Solution code here...
+  arr2.forEach(value => arr1.push(value));
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -159,7 +159,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should append the second array to the first', () => {
     const a = [1, 2, 3, 4];
     const b = [5, 6, 7, 8];
